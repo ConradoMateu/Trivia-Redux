900aecbeb8e2dcaad3279f490b7f1616301a456b
Conrado Mateu Gisbert <conradomateugisbert@gmail.com>
2021-01-06 16:26:30 +0100

    Initial Commit

---
+++ Trivial iOS/Trivial_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivial_iOSApp.swift
+//  Trivial iOS
+//
+//  Created by Conrado Mateu on 6/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Trivial_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Trivial iOSTests/Trivial_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Trivial_iOSTests.swift
+//  Trivial iOSTests
+//
+//  Created by Conrado Mateu on 6/1/21.
+//
+
+import XCTest
+@testable import Trivial_iOS
+
+class Trivial_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
